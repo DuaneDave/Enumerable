d795682541477891585e0ec96ffcfb6d1085885a
aeh1707d <abdelhadi.hireche@outlook.com>
2022-10-27 15:22:32 +0100

    Create myEnumrable.rb

--- myEnumerable.rb
+++ myEnumerable.rb
@@ -1,2 +1,17 @@
 module MyEnumerable
-end
+    def all?
+      each { |i| return false unless yield i }
+      true
+    end
+  
+    def any?
+      each { |i| return true if yield i }
+      false
+    end
+  
+    def filter
+      result = []
+      each { |i| result << i if yield i }
+      result
+    end
+  end
